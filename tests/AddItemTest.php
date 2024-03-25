0c02e4b9153dd3f94eca624c290805e79fe73054
mathis54300 <mathis54300fox@gmail.com>
2024-03-25 15:03:55 +0100

    test

--- tests/AddItemTest.php
+++ tests/AddItemTest.php
@@ -20,4 +20,24 @@ class AddItemTest extends TestCase
     {
         $this->assertFalse($this->addItem->isEmail('test@example'));
     }
+
+    public function testAddNewItem()
+    {
+        $allPostVars = [
+            'nom' => 'Test Name',
+            'email' => 'test@example.com',
+            'phone' => '1234567890',
+            'ville' => 'Test City',
+            'departement' => '1',
+            'categorie' => '1',
+            'title' => 'Test Title',
+            'description' => 'Test Description',
+            'price' => '100',
+            'psw' => 'password',
+            'confirm-psw' => 'password'
+        ];
+
+        $result = $this->addItem->addNewItem(null, null, null, $allPostVars);
+        $this->assertTrue($result);
+    }
 }
